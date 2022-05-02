6b6043277cbb1caeeae796bbe31fa2dabe79153b
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-05-02 14:14:47 +0100

    9/12 buttons connected and tets butotn for active functions added

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// document.querySelectorAll(".choice");
 
 class Character {
     constructor(Health, Stamina, Mana, Attack, Defense, Speed){
@@ -12,9 +11,9 @@ class Character {
         // pre-define starting stats
         this.level = 1,
         this.requiredExp = 10,
-        this.currentExp = 0;
+        this.currentExp = 0,
         
-        this.talentpoint = 1,
+        this.talentpoint = 0,
         this.moves = {
             basicATK: 10,
             strongATK: 25
@@ -25,71 +24,280 @@ class Character {
     gainExp(){
         setInterval(() => {
             this.currentExp += 1;
-            console.log(this.currentExp);
+            console.log(`${this.currentExp} EXP`);
             this.levelUp();
         }, 1000);
     }
     levelUp(){
         if(this.currentExp === this.requiredExp){
             this.level += 1;
+            this.talentpoint += 1;
             console.log("LEVEL UP");
-            console.log(this.level);
-            this.requiredExp += 10;
+            console.log(`You are now level-${this.level}`);
+            console.log(`You earned 1 Talent point. You now have ${this.talentpoint}`);
+            this.requiredExp += this.level*5;
             this.currentExp = 0;
         }
     }
-    useTalentPointChoice1(){
-        this.talentpoint -= 1;
+    
+    markOfTheSavage(){
         this.Attack +=10;
     }
-    useTalentPointChoice2(){
-        this.talentpoint -= 1;
+    markOfTheTitan(){
         this.Defense +=10;
     }
-    useTalentPointChoice3(){
-        this.talentpoint -= 1;
+    markOfTheRogue(){
         this.Speed +=10;
     }
-    useTalentPointChoice4(){
+    useTalentPointChoice1(){
+        console.log("You used a talent point");
         this.talentpoint -= 1;
+        console.log("You chose mark of the Savage");
+        this.markOfTheSavage();
+    }
+    useTalentPointChoice2(){
+        console.log("You used a talent point");
+        this.talentpoint -= 1;
+        console.log("You chose mark of the titan");
+        this.markOfTheTitan();
+    }
+    useTalentPointChoice3(){
+        console.log("You used a talent point");
+        this.talentpoint -= 1;
+        console.log("You chose mark of the rogue");
+        this.markOfTheRogue();
+    }
+    useTalentPointChoice4(){
         let shamansBlessing = setInterval(()=>{
             this.Health += 5;
-            console.log(this.Health)
-        }, 5000)
+            console.log(`You gained 5 Health, you have ${this.Health} Health`);
+        }, 5000);
+
+        console.log("You used a talent point");
+        this.talentpoint -= 1;
+        console.log("You chose Blessing of the Shaman");
+        console.log("Passive ability activated.")
         this.abilities.passive = {name: "shamansBlessing", action: shamansBlessing };
     }
     useTalentPointChoice5(){
-        this.talentpoint -= 1;
         let wizardsBlessing = setInterval(()=>{
             this.Mana += 5;
-            console.log(this.Mana)
+            console.log(`You gained 5 Mana. You have ${this.Mana} Mana`);
         }, 5000)
+        console.log("You used a talent point");
+        this.talentpoint -= 1;
+        console.log("You chose Blessing of the Wizard");
+        console.log("Passive ability activated.")
+        
         this.abilities.passive = {name: "wizardsBlessing", action: wizardsBlessing };
     }
     useTalentPointChoice6(){
-        this.talentpoint -= 1;
         let warriorsBlessing = setInterval(()=>{
             this.Stamina += 5;
-            console.log(this.Stamina)
+            console.log(`You gained 5 Stamina. You have ${this.Stamina} Stamina`);
         }, 5000)
+        console.log("You used a talent point");
+        this.talentpoint -= 1;
+        console.log("You chose Blessing of the Warrior");
+        console.log("Passive ability activated.")
+        
         this.abilities.passive = {name: "warriorsBlessing", action: warriorsBlessing };
     }
-}
+    // choices 7-9
+    //
+    elixirOfHealthTalent(){
+        this.Health += 25;
+        console.log(this.Health);
+        setTimeout(() => {
+            console.log("Cool-down ended");
+        }, 20000);
+    } 
+    useTalentPointChoice7(){
+        this.talentpoint -= 1;
+        this.abilities.active = {name: "Elixir of Health", action: this.elixirOfHealthTalent};
+        console.log(` ${this.abilities.active.name} added to activie abilities`);
+    }
+    elixirOfManaTalent(){
+        this.Mana += 25;
+        console.log(this.Health);
+        setTimeout(() => {
+            
+        }, 20000);
+    } 
+    useTalentPointChoice8(){
+        this.talentpoint -= 1;
+        this.abilities.active = {name: "Elixir of Mana ", action: this.elixirOfManaTalent};
+        console.log(` ${this.abilities.active.name} added to activie abilities`);
+    }
+    elixirOfValorTalent(){
+        this.Stamina += 25;
+        console.log(this.Stamina);
+        setTimeout(() => {
+            
+        }, 20000);
+    } 
+    useTalentPointChoice8(){
+        this.talentpoint -= 1;
+        this.abilities.active = {name: "Elixir of Valor ", action: this.elixirOfValorTalent};
+        console.log(` ${this.abilities.active.name} added to activie abilities`);
+    }
 
-let shaman = new Character(50, 5, 5, 5, 5, 5)
-let wizard = new Character(50, 5, 5, 5, 5, 5);
-let warrior = new Character(50, 5, 5, 5, 5, 5);
+    shamonicBloodRitualTalent(){
+        this.Health += 50;
+        this.Defense += 25;
+        console.log(this.Health);
+        console.log(this.Defense);
+            setTimeout(() => {
+                this.Defence -= 25;
+                console.log("Defence returned to normal");
+                console.log(this.Defence);
+            }, 10000);
+    }
+    useTalentPointChoice10(){
+        this.talentpoint -= 1;
+        this.abilities.active = {name: "Shamonic Blood Ritual", action: this.shamonicBloodRitualTalent};
+    }
+    darkWizardTransformationTalent(){
+        this.Mana += 50;
+        this.Attack += 25;
+        console.log(this.Mana);
+        console.log(this.Attack);
+            setTimeout(() => {
+                this.Attack -= 25;
+                console.log("Attack returned to normal");
+                console.log(this.Attack);
+            }, 10000);
+    }  
+    useTalentPointChoice11(){
+        this.talentpoint -= 1;
+        this.abilities.active = {name: "DarkWizardTransformation", action: this.darkWizardTransformationTalent};
+    }
+    beserkerMetamorphasisTalent(){
+        this.Stamina += 50;
+        this.Speed += 25;
+        console.log(this.Stamina);
+        console.log(this.Speed);
+            setTimeout(() => {
+                this.Speed -= 25;
+                console.log("Speed returned to normal");
+                console.log(this.Speed);
+            }, 10000);
+    }
+    useTalentPointChoice12(){
+        this.talentpoint -= 1;
+        this.abilities.active = {name: "BeserkerMetamorphasis", action: this.beserkerMetamorphasisTalent};
+    }    
+};
+
+let shaman = new Character(50, 6, 5, 5, 7, 6);
+let wizard = new Character(50, 5, 6, 7, 5, 6);
+let warrior = new Character(50, 6, 5, 7, 5, 6);
 // ============================================
 //  code testing
 // ============================================
+const r1c1 = document.getElementById("r1c1");
+const r1c2 = document.getElementById("r1c2");
+const r1c3 = document.getElementById("r1c3");
+const r2c1 = document.getElementById("r2c1");
+const r2c2 = document.getElementById("r2c2");
+const r2c3 = document.getElementById("r2c3");
+const r3c1 = document.getElementById("r3c1");
+const r3c2 = document.getElementById("r3c2");
+const r3c3 = document.getElementById("r3c3");
+r1c1.addEventListener("click", function(){
+    if(warrior.talentpoint >=1){
+        warrior.useTalentPointChoice1();
+        console.log(`Stats = ATK: ${warrior.Attack}, DEF: ${warrior.Defense}, SPD: ${warrior.Speed}`);
+    } else {
+        console.log(`You have ${warrior.talentpoint} talent points, you need 1 for this talent.`);
+    }
+}); 
+r1c2.addEventListener("click", function(){
+    if(warrior.talentpoint >=1){
+        warrior.useTalentPointChoice2();
+        console.log(`Stats = ATK: ${warrior.Attack}, DEF: ${warrior.Defense}, SPD: ${warrior.Speed}`);
+    } else {
+        console.log(`You have ${warrior.talentpoint} talent points, you need 1 for this talent.`);
+    }
+});
+r1c3.addEventListener("click", function(){
+    if(warrior.talentpoint >=1){
+        warrior.useTalentPointChoice3();
+        console.log(`Stats = ATK: ${warrior.Attack}, DEF: ${warrior.Defense}, SPD: ${warrior.Speed}`);
+    } else {
+        console.log(`You have ${warrior.talentpoint} talent points, you need 1 for this talent.`);
+    }
+});
+r2c1.addEventListener("click", function(){
+    if(warrior.talentpoint >=1){
+        warrior.useTalentPointChoice4();
+    } else {
+        console.log(`You have ${warrior.talentpoint} talent points, you need 1 TP for this talent.`);
+    }
+});
+r2c2.addEventListener("click", function(){
+    if(warrior.talentpoint >=1){
+        warrior.useTalentPointChoice5();
+    } else {
+        console.log(`You have ${warrior.talentpoint} talent points, you need 1 TP for this talent.`);
+    }
+});
+r2c3.addEventListener("click", function(){
+    if(warrior.talentpoint >=1){
+        warrior.useTalentPointChoice6();
+    } else {
+        console.log(`You have ${warrior.talentpoint} talent points, you need 1 TP for this talent.`);
+    }
+});
+// =======================================================
+const activeTest = document.getElementById("active-test");
+activeTest.addEventListener("click", function(){
+    warrior.elixirOfHealthTalent();
+    console.log(`Stats = HP: ${warrior.Health}, MP: ${warrior.Mana}, SP: ${warrior.Stamina}`);
+})
+// ======================================================
+
+r3c1.addEventListener("click", function(){
+    if(warrior.talentpoint >=1){
+        warrior.useTalentPointChoice7();
+        console.log(`Stats = HP: ${warrior.Health}, MP: ${warrior.Mana}, SP: ${warrior.Stamina}`);
+    } else {
+        console.log(`You have ${warrior.talentpoint} talent points, you need 1 TP for this talent.`);
+    }
+});
+r3c2.addEventListener("click", function(){
+    if(warrior.talentpoint >=1){
+        warrior.useTalentPointChoice8();
+        console.log(`Stats = HP: ${warrior.Health}, MP: ${warrior.Mana}, SP: ${warrior.Stamina}`);
+    } else {
+        console.log(`You have ${warrior.talentpoint} talent points, you need 1 TP for this talent.`);
+    }
+});
+r3c3.addEventListener("click", function(){
+    if(warrior.talentpoint >=1){
+        warrior.useTalentPointChoice8();
+        console.log(`Stats = HP: ${warrior.Health}, MP: ${warrior.Mana}, SP: ${warrior.Stamina}`);
+    } else {
+        console.log(`You have ${warrior.talentpoint} talent points, you need 1 TP for this talent.`);
+    }
+});
+
 //===========================================
-// use onece talents tetsing
+// use once talents tetsing
 // =====================================
-// console.log(warrior.talentpoint);
+
 // console.log(warrior.Attack);
 // warrior.useTalentPointChoice1();
 // console.log(warrior.talentpoint);
 // console.log(warrior.Attack);
+// console.log(warrior.talentpoint);
+// console.log(warrior.Stamina);
+// console.log(warrior.Speed);
+// warrior.useTalentPointChoice12();
+// // console.log(warrior.talentpoint);
+// console.log(warrior.abilities.active.name);
+// warrior.beserkerMetamorphasisTalent();
+
 //===========================================
 // passive talent testing 
 // =========================================
@@ -100,6 +308,20 @@ let warrior = new Character(50, 5, 5, 5, 5, 5);
 //===========================================
 // passive talent testing 
 // =========================================
-console.log(warrior.currentExp)
-console.log(warrior.requiredExp)
+
+
+
+console.log(`You're level-${warrior.level}`);
+console.log(`You need ${warrior.requiredExp} EXP to level-up`);
+console.log(`You have ${warrior.currentExp} EXP`);
+console.log(`You have ${warrior.talentpoint} Talent Points`);
+console.log(`Stats = ATK: ${warrior.Attack}, DEF: ${warrior.Defense}, SPD: ${warrior.Speed}`);
+
+// warrior.useTalentPointChoice1();
+console.log(`Stats = ATK: ${warrior.Attack}, DEF: ${warrior.Defense}, SPD: ${warrior.Speed}`);
+
+
 warrior.gainExp();
+
+
+// console.log(warrior.abilities.active.name);
