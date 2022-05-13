4af08a44edb43e58c2dbb62626cd22e4fbdfe8b4
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-05-13 11:37:49 +0100

    tidied up folders and restructured code

---
+++ classes.js
@@ -0,0 +1,47 @@
+// class definining
+class Sprite {
+    constructor({ position, image, frames = {col: 1, row: 1}}) { 
+        this.position = position
+        this.image = image
+        this.frames = frames
+
+        this.image.onload = () => {
+            this.width = this.image.width / this.frames.col
+            this.height = this.image.height / this.frames.row
+            console.log(this.width)
+            console.log(this.height)
+        }
+        
+    }
+
+    draw() {
+        c.drawImage(
+            this.image,
+            0,
+            0,
+            this.image.width / this.frames.col,
+            this.image.height / this.frames.row,
+            this.position.x,
+            this.position.y,
+            this.image.width / this.frames.col,
+            this.image.height / this.frames.row
+        )
+    } 
+}
+
+// define booundaries class
+class Boundary {
+    // static property for zoomed in map image (16px actual x 4 zoom)
+    static width = 64
+    static height = 64
+    constructor({position}) {
+        this.position = position
+        this.width = 64
+        this.height = 64
+    }
+
+    draw() {
+        c.fillStyle = "rgba(255, 0, 0, 1"
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+};
--- index.js
+++ index.js
@@ -13,22 +13,6 @@ for (let i = 0; i < collisions.length; i += 70){
    collisionsMap.push(collisions.slice(i, 70 + i))
 }
 
-// define booundaries class
-class Boundary {
-    // static property for zoomed in map image (16px actual x 4 zoom)
-    static width = 64
-    static height = 64
-    constructor({position}) {
-        this.position = position
-        this.width = 64
-        this.height = 64
-    }
-
-    draw() {
-        c.fillStyle = "rgba(255, 0, 0, 0"
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
-};
 
 // booundaries array for pushing boundary  
 const boundaries = [];
@@ -61,36 +45,7 @@ image.src = "images/map.png";
 const playerImage = new Image();
 playerImage.src = "images/char.png";
 
-// class definining
-class Sprite {
-    constructor({ position, velocity, image, frames = {col: 1, row: 1}}) { 
-        this.position = position
-        this.image = image
-        this.frames = frames
 
-        this.image.onload = () => {
-            this.width = this.image.width / this.frames.col
-            this.height = this.image.height / this.frames.row
-            console.log(this.width)
-            console.log(this.height)
-        }
-        
-    }
-
-    draw() {
-        c.drawImage(
-            this.image,
-            0,
-            0,
-            this.image.width / this.frames.col,
-            this.image.height / this.frames.row,
-            this.position.x,
-            this.position.y,
-            this.image.width / this.frames.col,
-            this.image.height / this.frames.row
-        )
-    } 
-}
 
 // creating class objects
 // player
@@ -107,8 +62,15 @@ const player = new Sprite({
 })
 // background
 
-
 const background = new Sprite({
+    position: {
+        x: offset.x, 
+        y: offset.y
+    },
+    image: image
+})
+
+const foreground = new Sprite({
     position: {
         x: offset.x, 
         y: offset.y
